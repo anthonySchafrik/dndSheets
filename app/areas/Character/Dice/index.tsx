d3f65d04d1eef6a463d80d29402f3dcd2f8cb350
Anthony <anthonyschafrik@gmail.com>
2021-09-28 23:13:34 -0400

    Added in dice assets as well as dispalying to imag

--- app/areas/Character/Dice/index.tsx
+++ app/areas/Character/Dice/index.tsx
@@ -1,14 +1,85 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Image, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import theme from '../../../theme';
 
 const Dice = () => {
   return (
-    <View>
-      <Text>This is the dice dothat</Text>
+    <View style={styles.screen}>
+      <Text>Select Multiplier</Text>
+      <TouchableOpacity
+        onPress={() => {
+          console.log('clicked d4');
+        }}>
+        <Image
+          style={styles.img}
+          source={require('../../../../assets/dice/d4.png')}
+        />
+      </TouchableOpacity>
+      <TouchableOpacity
+        onPress={() => {
+          console.log('clicked d6');
+        }}>
+        <Image
+          style={styles.img}
+          source={require('../../../../assets/dice/d6.png')}
+        />
+      </TouchableOpacity>
+      <TouchableOpacity
+        onPress={() => {
+          console.log('clicked d8');
+        }}>
+        <Image
+          style={styles.img}
+          source={require('../../../../assets/dice/d8.png')}
+        />
+      </TouchableOpacity>
+      <TouchableOpacity
+        onPress={() => {
+          console.log('clicked d10');
+        }}>
+        <Image
+          style={styles.img}
+          source={require('../../../../assets/dice/d10.png')}
+        />
+      </TouchableOpacity>
+      <TouchableOpacity
+        onPress={() => {
+          console.log('clicked percentile');
+        }}>
+        <Image
+          style={styles.img}
+          source={require('../../../../assets/dice/percentile.png')}
+        />
+      </TouchableOpacity>
+      <TouchableOpacity
+        onPress={() => {
+          console.log('clicked d12');
+        }}>
+        <Image
+          style={styles.img}
+          source={require('../../../../assets/dice/d12.png')}
+        />
+      </TouchableOpacity>
+      <TouchableOpacity
+        onPress={() => {
+          console.log('clicked d20');
+        }}>
+        <Image
+          style={styles.img}
+          source={require('../../../../assets/dice/d20.png')}
+        />
+      </TouchableOpacity>
     </View>
   );
 };
 
 export default Dice;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  screen: {
+    flex: 1,
+    backgroundColor: theme.background,
+    padding: 2,
+  },
+  img: { height: 75, width: 75 },
+});
